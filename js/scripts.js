3ae9ba4488d5b2f69ddde54e1eb9828ac0d49694
astop21 <antisrbtop@gmail.com>
2023-01-27 23:10:52 +0100

    changed playing method

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
-function myMove() {
-    let id = null;
-    const elem = document.getElementById("animate");   
-    let pos = 0;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame() {
-      if (pos == 350) {
-        clearInterval(id);
-      } else {
-        pos++; 
-        elem.style.top = pos + "px"; 
-        elem.style.left = pos + "px"; 
-      }
+let music_audio = new Audio("assets/snd/safet.mp3");
+
+function play_or_pause() { 
+    if(music_audio.paused){
+        music_audio.play(); 
     }
-  }
+    else{
+        music_audio.pause(); 
+    }
+} 
+
+const red_square = document.getElementById("animate");
+red_square.addEventListener("click", play_or_pause, false);
+
